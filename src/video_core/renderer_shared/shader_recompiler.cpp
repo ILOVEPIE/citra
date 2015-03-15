df697f78af391c6a00b5f5df80caf0781382004e
Patrick R Martin <patrick.martin.r@gmail.com>
2015-03-14 17:01:13 -0700

    Initial work on shader recompiler.

---
+++ src/video_core/renderer_shared/shader_recompiler.cpp
@@ -0,0 +1,23 @@
+
+// Copyright 2014 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include <nihstro/shader_bytecode.h>
+
+#include "../pica.h"
+#include "shader_recompiler.h"
+
+using nihstro::OpCode;
+using nihstro::Instruction;
+using nihstro::RegisterType;
+using nihstro::SourceRegister;
+using nihstro::SwizzlePattern;
+
+namespace Pica {
+
+namespace SSR {
+
+}
+
+}
